bef8478df9470d023f746cd5b4bbb7ee01e54368
hossensumon071 <hossensumon071@gmail.com>
2021-03-29 20:56:56 +0600

    amazing college theme wordpress

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+function myFirstFunction($name, $color){
+    echo "<p>Lorem ipsum dolor $name amet consectetur adipisicing $color. Dignissimos, aliquam?</p>";
+}
+myFirstFunction('amir','green');
+myFirstFunction('sumon', 'black');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
